3ea4e781414de6decb1fcf82c9c03faaac75a969
lanteiraph <santamulantei8@gmail.com>
2022-11-18 17:18:02 +0300

    feat: db add through  mongodn and mongoose

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,17 @@
-import Express from 'express'
+import Express from "express";
+import mongoose from "mongoose";
+const app = Express();
+const uri =
+  "mongodb+srv://lantei:lantei95@cluster0.aybaugd.mongodb.net/socialmedia?retryWrites=true&w=majority";
 
-const app = Express()
+mongoose.connect(uri, () => {
+  console.log("Connected to MongoDB");
+});
 
-app.listen(8080, ()=>{
-    console.log('Server up ad running');
-})
+app.use("/", (req, res) => {
+  res.send("Hello world");
+});
+
+app.listen(8080, () => {
+  console.log("Server up ad running");
+});
